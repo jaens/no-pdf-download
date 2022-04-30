8b2c195f654ea988839a5a510633980224d7d14b
Jaen <git@jaens.dev>
2022-04-30 20:45:16 +0300

    Have a separate `it()` block for each test
    This works better with IDEs such as VS Code.

--- test/test.js
+++ test/test.js
@@ -2,175 +2,193 @@ import { deepStrictEqual } from "assert";
 import * as headers from "../app/src/headers.js";
 
 describe("Handle Headers", () => {
-    testHandleHeaders("Empty", "", [], false);
-    testHandleHeaders(
-        "False positive",
-        "",
-        [
-            ["Server", "nginx/1.13.7"],
-            ["Content-Length", "456"],
-            ["Content-Encoding", "gzip"],
-            ["Content-Type", "text/html; charset=utf-8"],
-        ],
-        false
-    );
-    testHandleHeaders(
-        "False positive HTML",
-        "http://test.com/test.pdf",
-        [
-            ["Content-Disposition", "attachment"],
-            ["Content-Type", "text/html"],
-        ],
-        false
-    );
-    testHandleHeaders(
-        "False positive image",
-        "",
-        [
-            ["Content-Disposition", "attachment; filename=test.pdf"],
-            ["Content-Type", "image/jpeg"],
-        ],
-        false
-    );
-    testHandleHeaders(
-        "Normal PDF",
-        "",
-        [["Content-Type", "Application/PDF"]],
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "inline"],
-        ]
-    );
-    testHandleHeaders(
-        "Inline",
-        "",
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "inline"],
-        ],
-        false
-    );
-    testHandleHeaders(
-        "Attachment",
-        "",
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "ATTACHMENT"],
-        ],
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "inline"],
-        ]
-    );
-    testHandleHeaders(
-        "Attachment with filename",
-        "http://test.com/test.pdf",
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "attachment;filename=test.pdf"],
-        ],
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "inline;filename=test.pdf"],
-        ]
-    );
-    testHandleHeaders(
-        "Attachment and inline (invalid)",
-        "",
-        [
-            ["content-type", "application/pdf"],
-            ["Content-Disposition", "attachment; filename=test.pdf; inline"],
-        ],
-        [
-            ["content-type", "application/pdf"],
-            ["Content-Disposition", "inline; filename=test.pdf; inline"],
-        ]
-    );
-    testHandleHeaders(
-        "Charset",
-        "",
-        [["Content-Type", "application/pdf;charset=ISO-8859-1"]],
-        [
-            ["Content-Type", "application/pdf;charset=ISO-8859-1"],
-            ["Content-Disposition", "inline"],
-        ]
-    );
-    testHandleHeaders(
-        "x-PDF",
-        "",
-        [["Content-Type", "application/x-pdf"]],
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "inline"],
-        ]
-    );
-    testHandleHeaders(
-        "Image-PDF",
-        "",
-        [
-            ["Expires", "Sat, 27 Jan 2018 22:48:52 GMT"],
-            ["Content-Type", "image/pdf;charset=ISO-8859-1"],
-        ],
-        [
-            ["Expires", "Sat, 27 Jan 2018 22:48:52 GMT"],
-            ["Content-Type", "application/pdf;charset=ISO-8859-1"],
-            ["Content-Disposition", "inline"],
-        ]
-    );
-    testHandleHeaders(
-        "Octet-stream with PDF file name and invalid",
-        "",
-        [
-            ["Content-Type", "application/octet-stream"],
-            ["Content-Disposition", 'attachment;filename="te;t.pdf";filename=img.jpg'],
-        ],
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", 'inline;filename="te;t.pdf";filename=img.jpg'],
-        ]
-    );
-    testHandleHeaders(
-        "Octet-stream with image",
-        "http://test.com/test.pdf",
-        [
-            ["Content-Type", "application/octet-stream"],
-            ["Content-Disposition", "attachment;filename=image.jpg"],
-        ],
-        false
-    );
-    testHandleHeaders(
-        "Octet-stream with PDF in URL",
-        "http://test.com/test.PDF?1",
-        [["Content-Type", "application/OCTET-stream "]],
-        [
-            ["Content-Type", "application/pdf"],
-            ["Content-Disposition", "inline"],
-        ]
-    );
-    testHandleHeaders(
-        "Application/Force-Download with PDF file name(s)",
-        "http://test.com/02000",
-        [
-            ["Content-Disposition", "attachment; filename=\"ABC_2019.pdf\"; filename*=UTF-8''ABC_2019.pdf"],
-            ["Content-Type", "application/force-download"],
-        ],
-        [
-            ["Content-Disposition", "inline; filename=\"ABC_2019.pdf\"; filename*=UTF-8''ABC_2019.pdf"],
-            ["Content-Type", "application/pdf"],
-        ]
-    );
-    testHandleHeaders(
-        "Amazon S3 without content type with PDF file name(s)",
-        "http://some-bucket.s3.us-east-1.amazonaws.com/12345.pdf?response-content-disposition=attachment",
-        [
-            ["Content-Disposition", "attachment"],
-            ["Content-Type", ""],
-        ],
-        [
-            ["Content-Disposition", "inline"],
-            ["Content-Type", "application/pdf"],
-        ]
-    );
+    it("Empty", () => {
+        testHandleHeaders("", [], false);
+    });
+    it("False positive", () => {
+        testHandleHeaders(
+            "",
+            [
+                ["Server", "nginx/1.13.7"],
+                ["Content-Length", "456"],
+                ["Content-Encoding", "gzip"],
+                ["Content-Type", "text/html; charset=utf-8"],
+            ],
+            false
+        );
+    });
+    it("False positive HTML", () => {
+        testHandleHeaders(
+            "http://test.com/test.pdf",
+            [
+                ["Content-Disposition", "attachment"],
+                ["Content-Type", "text/html"],
+            ],
+            false
+        );
+    });
+    it("False positive image", () => {
+        testHandleHeaders(
+            "",
+            [
+                ["Content-Disposition", "attachment; filename=test.pdf"],
+                ["Content-Type", "image/jpeg"],
+            ],
+            false
+        );
+    });
+    it("Normal PDF", () => {
+        testHandleHeaders(
+            "",
+            [["Content-Type", "Application/PDF"]],
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "inline"],
+            ]
+        );
+    });
+    it("Inline", () => {
+        testHandleHeaders(
+            "",
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "inline"],
+            ],
+            false
+        );
+    });
+    it("Attachment", () => {
+        testHandleHeaders(
+            "",
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "ATTACHMENT"],
+            ],
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "inline"],
+            ]
+        );
+    });
+    it("Attachment with filename", () => {
+        testHandleHeaders(
+            "http://test.com/test.pdf",
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "attachment;filename=test.pdf"],
+            ],
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "inline;filename=test.pdf"],
+            ]
+        );
+    });
+    it("Attachment and inline (invalid)", () => {
+        testHandleHeaders(
+            "",
+            [
+                ["content-type", "application/pdf"],
+                ["Content-Disposition", "attachment; filename=test.pdf; inline"],
+            ],
+            [
+                ["content-type", "application/pdf"],
+                ["Content-Disposition", "inline; filename=test.pdf; inline"],
+            ]
+        );
+    });
+    it("Charset", () => {
+        testHandleHeaders(
+            "",
+            [["Content-Type", "application/pdf;charset=ISO-8859-1"]],
+            [
+                ["Content-Type", "application/pdf;charset=ISO-8859-1"],
+                ["Content-Disposition", "inline"],
+            ]
+        );
+    });
+    it("x-PDF", () => {
+        testHandleHeaders(
+            "",
+            [["Content-Type", "application/x-pdf"]],
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "inline"],
+            ]
+        );
+    });
+    it("Image-PDF", () => {
+        testHandleHeaders(
+            "",
+            [
+                ["Expires", "Sat, 27 Jan 2018 22:48:52 GMT"],
+                ["Content-Type", "image/pdf;charset=ISO-8859-1"],
+            ],
+            [
+                ["Expires", "Sat, 27 Jan 2018 22:48:52 GMT"],
+                ["Content-Type", "application/pdf;charset=ISO-8859-1"],
+                ["Content-Disposition", "inline"],
+            ]
+        );
+    });
+    it("Octet-stream with PDF file name and invalid", () => {
+        testHandleHeaders(
+            "",
+            [
+                ["Content-Type", "application/octet-stream"],
+                ["Content-Disposition", 'attachment;filename="te;t.pdf";filename=img.jpg'],
+            ],
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", 'inline;filename="te;t.pdf";filename=img.jpg'],
+            ]
+        );
+    });
+    it("Octet-stream with image", () => {
+        testHandleHeaders(
+            "http://test.com/test.pdf",
+            [
+                ["Content-Type", "application/octet-stream"],
+                ["Content-Disposition", "attachment;filename=image.jpg"],
+            ],
+            false
+        );
+    });
+    it("Octet-stream with PDF in URL", () => {
+        testHandleHeaders(
+            "http://test.com/test.PDF?1",
+            [["Content-Type", "application/OCTET-stream "]],
+            [
+                ["Content-Type", "application/pdf"],
+                ["Content-Disposition", "inline"],
+            ]
+        );
+    });
+    it("Application/Force-Download with PDF file name(s)", () => {
+        testHandleHeaders(
+            "http://test.com/02000",
+            [
+                ["Content-Disposition", "attachment; filename=\"ABC_2019.pdf\"; filename*=UTF-8''ABC_2019.pdf"],
+                ["Content-Type", "application/force-download"],
+            ],
+            [
+                ["Content-Disposition", "inline; filename=\"ABC_2019.pdf\"; filename*=UTF-8''ABC_2019.pdf"],
+                ["Content-Type", "application/pdf"],
+            ]
+        );
+    });
+    it("Amazon S3 without content type with PDF file name(s)", () => {
+        testHandleHeaders(
+            "http://some-bucket.s3.us-east-1.amazonaws.com/12345.pdf?response-content-disposition=attachment",
+            [
+                ["Content-Disposition", "attachment"],
+                ["Content-Type", ""],
+            ],
+            [
+                ["Content-Disposition", "inline"],
+                ["Content-Type", "application/pdf"],
+            ]
+        );
+    });
 });
 
 /* Helper Functions */
@@ -179,12 +197,11 @@ describe("Handle Headers", () => {
  * Tests header arrays. If expectedHeaders is set to `false` headers must not
  * change.
  *
- * @param {string} name
  * @param {string} url
  * @param {string[][]} requestHeaders
  * @param {string[][] | false} expectedHeaders
  */
-function testHandleHeaders(name, url, requestHeaders, expectedHeaders) {
+function testHandleHeaders(url, requestHeaders, expectedHeaders) {
     // Prepare expected headers
     if (expectedHeaders === false) {
         expectedHeaders = requestHeaders;
@@ -211,9 +228,7 @@ function testHandleHeaders(name, url, requestHeaders, expectedHeaders) {
 
     // Test
     const expectedArray = makeHeaderArray(expectedHeaders).sort(cmpHeaders);
-    it(name, () => {
-        deepStrictEqual(newHeaders, expectedArray);
-    });
+    deepStrictEqual(newHeaders, expectedArray);
 }
 
 /**
